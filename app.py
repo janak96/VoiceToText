3d8fcad5f8d174232a91684f737e5fb478ed0f96
janak96 <janakjoshi1996@gmail.com>
2023-09-21 23:21:54 +0530

    Chnages in py file

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ mic = pyaudio.PyAudio()
 stream = mic.open(rate=16000,channels=1,format=pyaudio.paInt16,input=True,frames_per_buffer=8192)
 stream.start_stream()
 
+# Lorem Ipsum
 
 # read the stream and make out the text
 while True:
